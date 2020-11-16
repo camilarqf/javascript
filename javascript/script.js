005b9f249c0d3cc4c604e966044f6ac49a5ad363
Camila Rocha de Queiroz Fonseca <camila.queiroz@slu.gdfnet.df>
2020-11-16 13:21:48 -0300

    aula 0201

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+//declarar variavel com o seu nome
+
+var nome = 'Camila';
+console.log(nome);
+
+//declarar variavel com o sua idade
+
+var idade = 30;
+console.log(idade);
+
+//declarar variavel com sua comida favorita e não atribuir valor
+
+var comida;
+console.log(comida);
+
+// atribuir valor a comida favorita
+comida = 'strogonoff';
+console.log(comida);
---
+++ javascript_2/script.js
@@ -0,0 +1,28 @@
+var nome = "teste2";
+var idade = 30;
+var variavel = null;
+
+console.log(typeof variavel);
+
+var nome =  'Camila';
+var sobrenome = 'Rocha';
+var nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+var frase = "teste com \"aspas\"";
+console.log(frase);
+
+var gols = 10;
+var frase2 = `romario fez ${gols *2} gols`;
+console.log(frase2);
+
+//declarar variavel com o seu nome
+
+var nome = 'Camila';
+console.log(nome);
+
+//declarar variavel com o sua idade
+
+var idade = 30;
+console.log(idade);
+
