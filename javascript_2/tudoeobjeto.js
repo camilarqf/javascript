5b91494ac5d1bacbe23ba20a3c33d307d0fa7c06
Camila Rocha de Queiroz Fonseca <camila.queiroz@slu.gdfnet.df>
2020-11-19 13:48:31 -0300

    Aula 0207

---
+++ javascript_2/tudoeobjeto.js
@@ -0,0 +1,16 @@
+var btn = document.querySelector('.btn');
+console.log(btn);
+
+
+//nomeie 3 propriedades ou metodos de strings
+
+var nome = 'Camila';
+nome.charAt(1);
+nome.fixed();
+nome.fontcolor('blue');
+
+//nomeie 5 propriedades ou métodos de elementos do DOM
+
+//busque na web um objeto (metodo) capaz de interagir com o clipboard
+
+//clipboard é a parte do seu computador que lida com o crtl c e ctrl v
